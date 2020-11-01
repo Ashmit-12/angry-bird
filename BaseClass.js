a1cc0910e912899ad520505b3b46b1d511340bc9
Ashmit-12 <ashmit12mittal@gmail.com>
2020-11-01 16:22:26 +0530

    final version

--- BaseClass.js
+++ BaseClass.js
@@ -17,7 +17,8 @@ class BaseClass{
         translate(this.body.position.x, this.body.position.y);
         rotate(angle);
         imageMode(CENTER);
-        image(this.image, 0, 0, this.width, this.height);
+       image(this.image, 0, 0,this.width,this.height);
+      //ellipse(0,0,this.width,this.height);
         pop();
       }
 }
